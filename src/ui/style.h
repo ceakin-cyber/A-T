54bb825cdf71b3aa815ad38777a88f47912e79b5
Chloe <chloe.e.eakin@gmail.com>
2026-09-20 20:45:27 -0400

    add tleage indicator (#152)

--- src/ui/style.h
+++ src/ui/style.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <imgui.h>
+
 namespace ui {
 
 // Applies the dark, flat, green-phosphor terminal style to the current ImGui context.
 void ApplyTerminalStyle();
+
+// Severity colors for text that should stand out from the green: amber for warnings, red for
+// serious problems.
+ImVec4 WarningColor();
+ImVec4 CriticalColor();
 
 } // namespace ui
